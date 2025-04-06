1f486f786da52f1b4268929877ec0055dda954d4
Thangarasu Dhanabal <thangarasudhanabal@gmail.com>
2025-04-06 16:59:25 -0400

    added BERT SQUAD-2 Base model sample.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
-import os
 import sys
-
-# Add the project root to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask import Flask
 from routes import setup_routes
 
